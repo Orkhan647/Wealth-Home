38d03c6b8d767c1bc24da17e7279ece0afa20063
Orkhan647 <okuw.k@mail.ru>
2026-06-21 14:48:34 +0400

    add js events

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**
+ * navbar toggle in mobile
+ */
+
+const /**{NoodeElement} */ $navbar = document.querySelector("[data-navbar]");
+const /**{NoodeElement} */ $navToggler =
+    document.querySelector("[data-nav-toggler");
+
+$navToggler.addEventListener("click", () => $navbar.classList.toggle("active"));
+
+
+/**
+ * Navbar scroll state
+ */
+
+const /**{NoodeElement} */ $header = document.querySelector("[data-header")
+
+window.addEventListener("scroll", e =>{
+ $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+ 
+});
