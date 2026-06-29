a413df7f0eaa75fc5921e5d5dddabb432c01211e
Orkhan647 <okuw.k@mail.ru>
2026-06-29 12:18:26 +0400

    create feature section(html)

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@
 
 const /**{NoodeElement} */ $navbar = document.querySelector("[data-navbar]");
 const /**{NoodeElement} */ $navToggler =
-    document.querySelector("[data-nav-toggler");
+    document.querySelector("[data-nav-toggler]");
 
 $navToggler.addEventListener("click", () => $navbar.classList.toggle("active"));
 
@@ -15,7 +15,7 @@ $navToggler.addEventListener("click", () => $navbar.classList.toggle("active"));
  * Navbar scroll state
  */
 
-const /**{NoodeElement} */ $header = document.querySelector("[data-header")
+const /**{NoodeElement} */ $header = document.querySelector("[data-header]");
 
 window.addEventListener("scroll", e =>{
  $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
@@ -27,7 +27,7 @@ window.addEventListener("scroll", e =>{
  * Add to favorite btn toggle
  */
 
-const /**{NoodeList} */ $toggleBtns = document.querySelectorAll("[data-toggle-btn]") 
+const /**{NoodeList} */ $toggleBtns = document.querySelectorAll("[data-toggle-btn]") ;
 
 $toggleBtns.forEach($toggleBtn =>{
 $toggleBtn.addEventListener("click",()=>{
